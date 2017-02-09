3decd32d0cf5ab3a6fddf7634ea6a71e0f6cceff
jolindse@hotmail.com <jolindse@hotmail.com>
2017-02-09 14:27:44 +0100

    Initial commit

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+export default class Login extends Component {
+
+    render() {
+        return (
+            <div id="login-root">
+
+            </div>
+        )
+    }
+}
